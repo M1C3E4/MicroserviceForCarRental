1d06a5aea7e876ecae70323294abe3f7cd913d74
Maciej Oklinski <mackoo.oklinski@gmail.com>
2022-09-22 19:28:17 +0100

    added stage red into TDD

--- src/test/java/com/example/microserviceforcarrental/acceptanceTests/AcceptanceTest.java
+++ src/test/java/com/example/microserviceforcarrental/acceptanceTests/AcceptanceTest.java
@@ -1,6 +1,5 @@
 package com.example.microserviceforcarrental.acceptanceTests;
 
-import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
