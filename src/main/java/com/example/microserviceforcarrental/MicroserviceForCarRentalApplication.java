db7cf64de412de0b6f87897ee762e5cdcea4de31
Maciej Oklinski <mackoo.oklinski@gmail.com>
2022-09-22 19:04:35 +0100

    initial commit

---
+++ src/main/java/com/example/microserviceforcarrental/MicroserviceForCarRentalApplication.java
@@ -0,0 +1,13 @@
+package com.example.microserviceforcarrental;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroserviceForCarRentalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MicroserviceForCarRentalApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/microserviceforcarrental/MicroserviceForCarRentalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.microserviceforcarrental;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroserviceForCarRentalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
